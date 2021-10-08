14974a8b11c474aaad2a85c514d72eee0906ae19
Ashley Swank <Ashley@Psychics-MacBook-Pro.local>
2021-10-08 19:12:03 -0400

    WIP login/auth set up

---
+++ controllers/users.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+
+
+// INDEX
+router.get("/", (req, res) => {
+    res.render('index.ejs')
+})
+// NEW
+router.get("/new", (req, res) => {
+    res.render("users/new.ejs", {
+      currentUser: req.session.currentUser,
+    })
+  })
+// DELETE
+
+// UPDATE
+
+// CREATE
+router.post("/", (req, res) => {
+    //overwrite the user password with the hashed password, then pass that in to our database
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+  
+    User.create(req.body, (error, createdUser) => {
+      res.redirect("/")
+    })
+  })
+
+// EDIT
+
+// SHOW
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const morgan = require('morgan')
+const userController = require("./controllers/users")
+const sessionsController = require("./controllers/sessions")
+const session = require("express-session")
+
+
+const app = express()
+
+
+require("dotenv").config()
+const PORT = process.env.PORT
+const DATABASE_URL = process.env.DATABASE_URL
+
+// Dependencies
+const methodOverride = require("method-override")
+
+
+
+// Database Configuration
+mongoose.connect(DATABASE_URL)
+
+// Database Connection Error / Success
+const db = mongoose.connection
+db.on("error", (err) => console.log("CODE BLUE! CODE BLUE!"))
+db.on("connected", () => console.log("What seems to be the problem?"))
+db.on("disconnected", () => console.log("Call back in the morning to let us know how you're feeling"))
+
+// Middleware
+app.use(express.urlencoded({ extended: false }))
+app.use(
+    session({
+      secret: process.env.SECRET,
+      resave: false,
+      saveUninitialized: false,
+    })
+  )
+  app.use(methodOverride("_method"))
+
+
+app.use("/users", userController)
+app.use("/sessions", sessionsController)
+
+// Routes / Controllers
+
+ app.get("/", (req, res) => {
+  res.render("index.ejs", {
+    currentUser: req.session.currentUser,
+  })
+})
+app.listen(PORT, () => console.log('The Doctor will see you now'))
